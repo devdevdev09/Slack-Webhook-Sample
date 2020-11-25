e3bddada98576554e4a2f534cc7c20fb79dc5359
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-11-25 20:56:50 +0900

    first commit

---
+++ src/main/java/com/sample/slack/slackwebhooks/SlackWebhooksApplication.java
@@ -0,0 +1,13 @@
+package com.sample.slack.slackwebhooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackWebhooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackWebhooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/slack/slackwebhooks/SlackWebhooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.slack.slackwebhooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackWebhooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
